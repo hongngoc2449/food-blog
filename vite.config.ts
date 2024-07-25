1a3d53311dbad6c2a0aa3edae516613a447a3b43
hongngocdoan <hongngoc2449@gmail.com>
2024-07-25 18:17:33 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages'
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    ghPages(),
+  plugins: [react()
+    
   ],
-  base: '/food-blog/',
+  // base: '/food-blog/',
 })
